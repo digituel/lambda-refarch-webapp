ec9386f8c32f44481d6515530323205e01f1f930
David Cox <davidc@Davids-MacBook-Pro.local>
2025-07-16 18:41:46 -0400

    more changes to deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -x
 [ -z "$STACK_NAME" ] && echo "Please specify STACK_NAME environment variable" && exit 1;
 [ -z "$AWS_DEFAULT_REGION" ] && echo "Please specify AWS_DEFAULT_REGION environment variable" && exit 1;
 
-sam build --use-container
+sam build --use-container false
 sam deploy --guided --stack-name $STACK_NAME
 
 export AWS_COGNITO_REGION=$AWS_DEFAULT_REGION
